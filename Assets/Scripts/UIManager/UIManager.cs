a8b0ed3f4383804393551407ea84ff1073974f02
ThuongDev12 <cuong.0907062600@gmail.com>
2025-02-19 00:20:52 +0700

    Create UI Shop

--- Assets/Scripts/UIManager/UIManager.cs
+++ Assets/Scripts/UIManager/UIManager.cs
@@ -1,4 +1,6 @@
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -6,6 +8,13 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] private GameObject _playGamePanel;
     [SerializeField] private GameObject _settingsPanel;
+
+    [Header("===Shop Panel===")]
+    [SerializeField] private GameObject _shopPanel;
+    public GameObject[] _tabs;
+    public Image[] _tabBtns;
+    public Sprite _inactiveTabBg, _activeTabBg;
+    public Vector2 _inactiveTabBtnSize, _activeTabBtnSize;
 
     private void Awake()
     {
@@ -23,6 +32,7 @@ public class UIManager : MonoBehaviour
     {
         // Ẩn ban đầu bằng scale 0
         if (_settingsPanel != null) _settingsPanel.transform.localScale = Vector3.zero;
+        //if (_shopPanel != null) _shopPanel.transform.localScale = Vector3.zero;
     }
 
     //Play Game Panel
@@ -54,5 +64,43 @@ public class UIManager : MonoBehaviour
             LeanTween.scale(_settingsPanel, Vector3.zero, 0.3f).setEase(LeanTweenType.easeInBack)
                 .setOnComplete(() => _settingsPanel.SetActive(false));
         }
+    }
+
+    //Shop Panel
+    public void OpenShopPanel()
+    {
+        if (_shopPanel != null)
+        {
+            _shopPanel.SetActive(true);
+            _shopPanel.transform.localScale = Vector3.zero;
+            LeanTween.scale(_shopPanel, Vector3.one, 0.3f).setEase(LeanTweenType.easeOutBack);
+        }
+    }
+
+    public void CloseShopPanel()
+    {
+        if (_shopPanel != null)
+        {
+            LeanTween.scale(_shopPanel, Vector3.zero, 0.3f).setEase(LeanTweenType.easeInBack)
+                .setOnComplete(() => _shopPanel.SetActive(false));
+        }
+    }
+
+    //Tab Shop
+    public void SwitchToTab(int TabID)
+    {
+        foreach (GameObject go in _tabs)
+        {
+            go.SetActive(false);
+        }
+        _tabs[TabID].SetActive(true);
+
+        foreach (Image im in _tabBtns)
+        {
+            im.sprite = _inactiveTabBg;
+            im.rectTransform.sizeDelta = _inactiveTabBtnSize;
+        }
+        _tabBtns[TabID].sprite = _activeTabBg;
+        _tabBtns[TabID].rectTransform.sizeDelta = _activeTabBtnSize;
     }
 }
